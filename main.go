ea66eb356f51deeef8e5b87d8f11d82df59686e2
안영진 <anyoungjin0106@kbu.ac.kr>
2025-02-21 03:27:45 +0900

    add ads

--- main.go
+++ main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	Ads := os.Getenv("Ads")
+	if Ads == "" {
+		Ads = "Default Ad Text"
+	}
+
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
 	r.GET("/", func(ctx *gin.Context) {
-		ctx.File("index.html")
+		ctx.HTML(http.StatusOK, "index.html", gin.H{
+			"Ads": Ads,
+		})
 	})
 	r.POST("/privacy", func(ctx *gin.Context) {
 		serviceName := ctx.PostForm("service_name")
@@ -22,12 +30,15 @@ func main() {
 			"CompanyName":   companyName,
 			"Email":         email,
 			"CollectedData": collectedData,
+			"Ads":           Ads,
 		}
 		ctx.HTML(http.StatusOK, "result.html", data)
 
 	})
 	r.NoRoute(func(ctx *gin.Context) {
-		ctx.File("404.html")
+		ctx.HTML(http.StatusOK, "404.html", gin.H{
+			"Ads": Ads,
+		})
 	})
 	r.Run(":8000")
 }
